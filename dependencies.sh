91e2fd57b91a16f87237a244aac1aa312ffc8a70
ailrk <jimmy123good@hotmail.com>
2018-04-24 13:53:08 -0700

    updated dependencies setup shell

---
+++ dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip install Flask_Wtf
+pip install Flask_Sqlalchemy
+pip install Flask_Script
+pip install Flask_Bootstrap
+pip install Flask_Migrate
--- setup.sh
+++ setup.sh
@@ -3,7 +3,3 @@ export FLASK_APP=lip.py
 export FLASK_DEBUG=1
 export SECRET_KEY=currychicken
 
-
-pip install Flask_Wtf
-pip install Flask_Sqlalchemy
-pip install Flask_Script
