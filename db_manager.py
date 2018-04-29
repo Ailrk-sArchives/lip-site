d1ab154c06b5f7d9942dfe06211d1306549ac9c9
Lucas <stillwwater@gmail.com>
2018-04-28 17:05:24 -0700

    add functionality to db_manager

--- db_manager.py
+++ db_manager.py
@@ -7,37 +7,65 @@ class Manager():
     def __init__(self):
         self.env = {
             'add': lambda callback: self.add(callback),
+            'get': lambda callback: self.get(callback),
+            'remove': lambda callback: self.remove(callback),
+            'edit': lambda callback: self.edit(callback),
             'hashpass': lambda rawpass: self.hashpass(rawpass)
         }
 
-        self.data_objects = {
-            'category': lambda: self.category(),
-            'user': lambda: self.user(),
-            'article': lambda: self.article()
+        self.add_callbacks = {
+            'category': lambda: self.new_category(),
+            'user': lambda: self.new_user(),
+            'article': lambda: self.new_article()
+        }
+
+        self.get_callbacks = {
+            'category': lambda args: Category.get_category(args[0]),
+            'categories': lambda args: Category.get_many_categories(),
+            'user': lambda args: User.get_user_by_name(args[0]),
+            'article': lambda args: Article.get_article(args[0]),
+            'articles': lambda args: Article.get_many_articles_by_category(args[0])
+        }
+
+        self.remove_callbacks = {
+            'user': lambda args: User.delete_user(args[0])
+        }
+
+        self.edit_callbacks = {
+            'article': lambda args: Article.edit_article(args[0], args[1], args[2])
         }
 
     def run(self, instruction, data_object, args):
         self.data_args = args
-        self.env[instruction](self.data_objects[data_object])
+        self.env[instruction](data_object)
         db.session.commit()
 
     def hashpass(self, rawpass):
         return hashlib.sha512(rawpass.encode('utf-8')).hexdigest()
 
     def add(self, callback):
-        res = callback()
+        res = self.add_callbacks[callback]()
         print(res)
         db.session.add_all([res])
 
-    def category(self):
+    def get(self, callback):
+        print(self.get_callbacks[callback](self.data_args))
+
+    def edit(self, callback):
+        print(self.edit_callbacks[callback](self.data_args))
+
+    def remove(self, callback):
+        print(self.remove_callbacks[callback](self.data_args))
+
+    def new_category(self):
         return Category(category=self.data_args[0])
 
-    def user(self):
+    def new_user(self):
         return User(username=self.data_args[0], \
             password_hash=self.env['hashpass'](self.data_args[1]), \
             email=self.data_args[2])
 
-    def article(self):
+    def new_article(self):
         return Article(title=self.data_args[0], \
             rawcontent=self.data_args[1], \
             content=markdown.markdown(self.data_args[1]), \
